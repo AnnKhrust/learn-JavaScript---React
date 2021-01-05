553da79d8b1cffe5008b8adbed5d278e04fe80e7
AnnKhrust <anna.khrust@gmail.com>
2021-01-05 20:37:00 +0300

    homework #3
    objest's metods

--- index.js
+++ index.js
@@ -1,81 +1,78 @@
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
-
-    while (numberOfFilms == '' || numberOfFilms == 0 || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
-    }
-}
-
-start();
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-}
-
-function rememberMyFilms(){
-for (let i = 0; i < 2; i++) {
+    privat: false,
+    start: function() {
+        personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", '');
     
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = +prompt('На сколько оцените его?');
-          if ( a != null && b != null && a != "" && b != '' && a.length < 50 ) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else { 
-            console.log('error');
-            i--;
+        while (personalMovieDB.count == '' || personalMovieDB.count == 0 || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt("Сколько фильмов вы уже посмотрели?", '');
         }
-}
-}
+    },
+    rememberMyFilms: function() {
+        for (let i = 0; i < 2; i++) {            
+            const a = prompt('Один из последних просмотренных фильмов?', ''),
+                  b = +prompt('На сколько оцените его?');
+                  if ( a != null && b != null && a != "" && b != '' && a.length < 50 ) {
+                    personalMovieDB.movies[a] = b;
+                    console.log('done');
+                } else { 
+                    console.log('error');
+                    i--;
+                }
+        }
+        },
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10) {
+            alert("Просмотрено довольно мало фильмов");
+        } else if (personalMovieDB.count < 30 && personalMovieDB.count >= 10) {
+            alert("Вы классический зритель");
+        } else if (personalMovieDB.count >= 30) {
+            alert("Вы киноман");
+        } else {
+            alert("Произошла ошибка");
+        }
+    },
+    showMyDB: function(hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    writeYourGenres: function() {
+        for (let i=0; i < 3; i++) {
 
-rememberMyFilms();
+            let genre = prompt(`Ваш любимый жанр под номером ${i + 1}`).toLowerCase;
 
-function detectPersonalLevel(){
-if (personalMovieDB.count < 10) {
-    alert("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count < 30 && personalMovieDB.count >= 10) {
-    alert("Вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
-    alert("Вы киноман");
-} else {
-    alert("Произошла ошибка");
-}
-}
+            if (genre == '' || genre == null) {
+                i--;
+            } else {
+                personalMovieDB.genres[i] = genre;
+            }                        
+        }
 
-detectPersonalLevel();
+        personalMovieDB.genres.forEach((item, i) => console.log(`"Любимый жанр #${i + 1} - это ${item}"`))
+    },
+    toggleVisibleMyDB: function() {
+        if (personalMovieDB.privat) {
+            personalMovieDB.privat = false;
+        } else {
+            personalMovieDB.privat = true;
+        }
 
-
-
-function showMyDB() {
-    if (personalMovieDB.privat == false) {
-        console.log(personalMovieDB)
     }
-
 }
-
-showMyDB();
-
-
-function writeYourGenres() {
-    for (let i=0; i < 3; i++) {
-        personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i + 1}`);
-    }
-}
-
-writeYourGenres();
-
-
 
 
 /* Задание на урок:
 
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+*/
 
-P.S. Функции вызывать не обязательно*/
+'use strict';
+
+// Код возьмите из предыдущего домашнего задания
